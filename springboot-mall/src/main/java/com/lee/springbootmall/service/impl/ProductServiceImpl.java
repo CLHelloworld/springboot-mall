2fa54c8a342362ab9035bcec41688c348df4fbaf
LEE <bnb00176248@gmail.com>
2023-12-25 23:52:37 +0800

    商品功能 - 實作查詢商品

--- springboot-mall/src/main/java/com/lee/springbootmall/service/impl/ProductServiceImpl.java
+++ springboot-mall/src/main/java/com/lee/springbootmall/service/impl/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ProductServiceImpl implements ProductService {
+
     @Autowired
     private ProductDao productDao;
     @Override
